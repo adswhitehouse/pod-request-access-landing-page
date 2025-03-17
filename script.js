095089064f9b7590f4dabb978c545ec17d329126
Adam Whitehouse <adswhitehouse95@gamil.com>
2025-03-17 20:35:51 +0000

    error message displays when invalid email is entered

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Variables
+let formInput = document.querySelector(".jsFormInput");
+let formBtn = document.querySelector(".jsFormBtn");
+let errorMsg = document.querySelector(".jsErrorMsg");
+let form = document.querySelector(".jsForm");
+
+let errorMsgTimeout;
+let isErrorMsgActive = false;
+formBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!formInput.value.includes("@")) {
+    errorMsg.classList.add("error-msg-visibility");
+    form.classList.add("error-border");
+    if (!isErrorMsgActive) {
+      isErrorMsgActive = true;
+      errorMsgTimeout = setTimeout(() => {
+        errorMsg.classList.remove("error-msg-visibility");
+        form.classList.remove("error-border");
+        isErrorMsgActive = false;
+      }, 2000);
+    } else {
+      clearTimeout(errorMsgTimeout);
+      errorMsgTimeout = setTimeout(() => {
+        errorMsg.classList.remove("error-msg-visibility");
+        form.classList.remove("error-border");
+        isErrorMsgActive = false;
+      }, 2000);
+    }
+  }
+});
